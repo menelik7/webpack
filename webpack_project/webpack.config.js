d1f7d792bc0c3a7f0bf0dacbc6d4cbbb575fc500
Menelik Tefera <menelik.tefera@ssf.no>
2023-05-01 13:02:37 +0200

    Adding rimraf to build process

--- webpack_project/webpack.config.js
+++ webpack_project/webpack.config.js
@@ -18,17 +18,35 @@ const VENDOR_LIBS = [
 module.exports = {
   entry: {
     bundle: './src/index.js',
-    vendor: VENDOR_LIBS,
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name].[chunkhash].js',
   },
   mode: 'development',
   optimization: {
     splitChunks: {
       chunks: 'all',
+      // minSize: 20000,
+      // minRemainingSize: 0,
+      // minChunks: 1,
+      // maxAsyncRequests: 30,
+      // maxInitialRequests: 30,
+      // enforceSizeThreshold: 50000,
+      // cacheGroups: {
+      //   defaultVendors: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     priority: -10,
+      //     reuseExistingChunk: true,
+      //   },
+      //   default: {
+      //     minChunks: 2,
+      //     priority: -20,
+      //     reuseExistingChunk: true,
+      //   },
+      // },
     },
+    runtimeChunk: { name: 'manifest' },
   },
   module: {
     rules: [
